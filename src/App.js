64be4b8ec054145ea0f2a30bcfb122a9b38804c0
Subalakshmi-Mohandoss <subalakshmijmk@gmail.com>
2023-11-08 16:20:12 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import Button from './Components/Button';
-
-function App() {
-  return(
-
-<div className="flex space-x-4 my-4">
+//import HelloWorld from './Day2/HelloWorld';
+//import ToggleMessage from './Day2/ToggleMessage';
+ function App() {
+   return(
+//    // <HelloWorld></HelloWorld>
+//    //<ToggleMessage></ToggleMessage>
+ <div className="flex space-x-4 my-4">
   <Button  color="green">Green</Button>
   <Button color="red">Red</Button>
   <Button  color="blue">Blue</Button>
   <Button color="yellow">Yellow</Button>
-  </div>
-  );
-}
+  </div> 
+   );
+};
 export default App;
