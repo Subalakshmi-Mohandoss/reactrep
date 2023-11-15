14a0a204c8303b8d8031d551487739a17dc3eeb9
Subalakshmi-Mohandoss <subalakshmijmk@gmail.com>
2023-11-15 15:42:28 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -14,31 +14,48 @@
 //    );
 // };
 // export default App;
-import { useState } from "react";
-import Cart from "./Day3/CoffeeShop/Cart";
-import Menu from "./Day3/CoffeeShop/Menu";
-import Navbar from "./Day3/CoffeeShop/Navbar";
-import categories from "./Day3/CoffeeShop/Products";
+import Counter from "./Day3/Counter";
 
-function App() {
-  const [activeCategory, setActiveCategory] = useState(categories[0]);
-  const [products, setProducts] = useState([]);
-
-  const handleClick = (index) => {
-    setActiveCategory(categories[index]);
-  };
-
-  const handleAddToCart = (item) => {
-    setProducts([...products, item]);
-  };
-
-  return (
-    <div className="space-y-4 my-4">
-      <Navbar category={categories} handleClick={handleClick} />
-      <Menu activeCategory={activeCategory} handleAddToCart={handleAddToCart} />
-      <Cart products={products} />
-    </div>
-  );
-}
-
+function App(){
+   return(
+      <div>
+         <Counter></Counter>
+      </div>
+   );
+};
 export default App;
+
+
+
+
+
+
+// import { useState } from "react";
+// import Cart from "./Day3/CoffeeShop/Cart";
+// import Menu from "./Day3/CoffeeShop/Menu";
+// import Navbar from "./Day3/CoffeeShop/Navbar";
+// import categories from "./Day3/CoffeeShop/Products";
+
+// function App() {
+//   const [activeCategory, setActiveCategory] = useState(categories[0]);
+//   const [products, setProducts] = useState([]);
+
+//   const handleClick = (index) => {
+//     setActiveCategory(categories[index]);
+//   };
+
+//   const handleAddToCart = (item) => {
+//     setProducts([...products, item]);
+//   };
+
+//   return (
+//     <div className="space-y-4 my-4">
+//       <Navbar category={categories} handleClick={handleClick} />
+//       <Menu activeCategory={activeCategory} handleAddToCart={handleAddToCart} />
+//       <Cart products={products} />
+//     </div>
+//   );
+// }
+
+// export default App;
+
